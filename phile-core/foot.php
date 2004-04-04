48baa260f1cd345dd05150be01d1d1b12f03a5ee
David Moreno <damog@damog.net>
2004-04-04 02:09:44 +0000

    Modificada la URL del proyecto Phile ;)

--- phile-core/foot.php
+++ phile-core/foot.php
@@ -3,5 +3,8 @@
 <td>
 <? echo $_SERVER['SERVER_SOFTWARE']." <b>@</b> ".$_SERVER['SERVER_NAME']; ?>
 </td>
-<td align="right"><font size=1>Powered by <i><a href="http://phile.red-libre.org">phile</a></i></td>
+<td align="right"><font size=1>Powered by 
+<i>
+<a href="http://phile.sourceforge.net">phile</a>
+</i></td>
 </tr></table>
